71e8ca18e23218a15048222e8f692114be920ac6
Jimmy Truong <jimmytruong@mines.edu>
2023-04-24 20:28:09 -0600

    oops

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "jqt-designs.github.io",
-  base: "/personal"
+	site: "jqt-designs.github.io",
+	base: "/personal",
 	integrations: [mdx(), sitemap(), tailwind()],
 });
