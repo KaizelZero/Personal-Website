b33072d06899ec1942c67d579f85118576451520
Jimmy Truong <jimmytruong@mines.edu>
2023-04-24 21:08:06 -0600

    revert link stuff bc i bought a domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jqt-designs.github.io",
-	base: "/personal",
+	site: "https://jimmyt.dev",
 	integrations: [mdx(), sitemap(), tailwind()],
 });
