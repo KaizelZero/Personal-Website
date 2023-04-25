fd46299de657029281606a5f94da772c35e93163
Jimmy Truong <jimmytruong@mines.edu>
2023-04-24 20:27:11 -0600

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "jqt-designs.github.io",
+  base: "/personal"
+	integrations: [mdx(), sitemap(), tailwind()],
 });
